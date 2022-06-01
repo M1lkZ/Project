b948ea436394b0aeb6088e22182f6ac04dac879d
M1lkZ <nickolasmarwin@gmail.com>
2022-06-01 22:04:02 +0300

    window reset

--- Sized Block/index.js
+++ Sized Block/index.js
@@ -1,6 +1,10 @@
 let block = document.getElementById("block")
 let range = document.getElementById("range")
 
+window.onload = function () {
+    range.value = "10";
+}
+
 range.oninput = function () {
     string_val = parseInt(range.value, 10).toString(10)
     text = string_val + "%"
