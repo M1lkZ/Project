9f26882a521e66dfa2f09709bb9d8bca9bf94494
M1lkZ <nickolasmarwin@gmail.com>
2022-08-04 00:41:16 +0300

    format optimizations

--- Js/index.js
+++ Js/index.js
@@ -1,70 +1,72 @@
 range1.oninput = function () {
-    let val = document.getElementById("range1").value
-    let textplace = document.getElementById("textplace1")
-    if (val <= 9) {
-        textplace.value = "00" + val
-    } else if (val <= 99) {
-        textplace.value = "0" + val
-    } else {
-        textplace.value = val
-    }
-}
+  let val = document.getElementById("range1").value;
+  let textplace = document.getElementById("textplace1");
+  if (val <= 9) {
+    textplace.value = "00" + val;
+  } else if (val <= 99) {
+    textplace.value = "0" + val;
+  } else {
+    textplace.value = val;
+  }
+};
 range2.oninput = function () {
-    let val = document.getElementById("range2").value
-    let textplace = document.getElementById("textplace2")
-    if (val <= 9) {
-        textplace.value = "00" + val
-    } else if (val <= 99) {
-        textplace.value = "0" + val
-    } else {
-        textplace.value = val
-    }
-}
+  let val = document.getElementById("range2").value;
+  let textplace = document.getElementById("textplace2");
+  if (val <= 9) {
+    textplace.value = "00" + val;
+  } else if (val <= 99) {
+    textplace.value = "0" + val;
+  } else {
+    textplace.value = val;
+  }
+};
 range3.oninput = function () {
-    let val = document.getElementById("range3").value
-    let textplace = document.getElementById("textplace3")
-    if (val <= 9) {
-        textplace.value = "0" + val
-    } else {
-        textplace.value = val
-    }
-}
+  let val = document.getElementById("range3").value;
+  let textplace = document.getElementById("textplace3");
+  if (val <= 9) {
+    textplace.value = "0" + val;
+  } else {
+    textplace.value = val;
+  }
+};
 range4.oninput = function () {
-    let val = document.getElementById("range4").value
-    let textplace = document.getElementById("textplace4")
-    if (val <= 9) {
-        textplace.value = "0" + val
-    } else {
-        textplace.value = val
-    }
-}
+  let val = document.getElementById("range4").value;
+  let textplace = document.getElementById("textplace4");
+  if (val <= 9) {
+    textplace.value = "0" + val;
+  } else {
+    textplace.value = val;
+  }
+};
 ProgressRange.oninput = function () {
-    let val = document.getElementById("ProgressRange").value
-    let textplace = document.getElementById("progress")
-    textplace.value = val
-}
+  let val = document.getElementById("ProgressRange").value;
+  let textplace = document.getElementById("progress");
+  textplace.value = val;
+};
 
-let flexSort = document.getElementById("flex-sort")
-let flag = 1
+let flexSort = document.getElementById("flex-sort");
+let flag = 1;
 
 flexSort.onclick = function () {
-    const allFlexItems = document.querySelectorAll('div[class *= flex-item]');
-    const flexItems = Array.from(allFlexItems);
-    flexItems.sort(function (a, b) {
-        return parseInt(window.getComputedStyle(b).getPropertyValue('height')) - parseInt(window.getComputedStyle(a).getPropertyValue('height'));
-    });
-    if (flag == 1) {
-        for (let i = 1; i < flexItems.length + 1; i++) {
-            flexItems[i].style.order = i;
-        }
-        flexSort.innerText = 'Unsort flex containers';
-        flag = 0;
-    } else {
-        for (let i = 1; i < flexItems.length + 1; i++) {
-            flexItems[i].style.order = "";
-        }
-        flexSort.innerText = 'Sort flex containers';
-        flag = 1;
+  const allFlexItems = document.querySelectorAll("div[class *= flex-item]");
+  const flexItems = Array.from(allFlexItems);
+  flexItems.sort(function (a, b) {
+    return (
+      parseInt(window.getComputedStyle(b).getPropertyValue("height")) -
+      parseInt(window.getComputedStyle(a).getPropertyValue("height"))
+    );
+  });
+  if (flag == 1) {
+    for (let i = 1; i < flexItems.length + 1; i++) {
+      flexItems[i].style.order = i;
     }
-
-}
+    flexSort.innerText = "Unsort flex containers";
+    flag = 0;
+  } else {
+    for (let i = 1; i < flexItems.length + 1; i++) {
+      flexItems[i].style.order = "";
+    }
+    flexSort.innerText = "Sort flex containers";
+    flag = 1;
+  }
+};
